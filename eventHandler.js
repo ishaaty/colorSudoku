085ff488fe749a13308f67fd06c16a3ee6a029a4
ishaaty <ishatyagi888@gmail.com>
2022-07-21 12:31:09 -0400

    starting to make solutions for sudoku puzzles

--- eventHandler.js
+++ eventHandler.js
@@ -4,15 +4,13 @@ window.addEventListener("load", function () {
     grid.replaceColor("white");
     grid.makeColorGrid(0, 0, canvas.height, canvas.width, 40);
     grid.makeColorGrid(0, 0, canvas.height, canvas.width, 120, 3);
+    grid.replaceColor("red");
 });
 canvas.addEventListener("click", function (event) {
     for (var _i = 0, squaresList_1 = squaresList; _i < squaresList_1.length; _i++) {
         var s = squaresList_1[_i];
-        if ((event.offsetX >= s.xCoordinate - 40 && event.offsetX <= s.xCoordinate) && (event.offsetY >= s.yCoordinate - 40 && event.offsetY <= s.yCoordinate)) {
-            s.makeColorGrid(s.xCoordinate - 37, s.yCoordinate - 37, s.xCoordinate - 2, s.yCoordinate - 2, 1, 1);
-        }
-        else if ((event.offsetX >= s.xCoordinate && event.offsetX <= s.xCoordinate + 40) && (event.offsetY >= s.yCoordinate && event.offsetY <= s.yCoordinate + 40)) {
-            s.makeColorGrid(s.xCoordinate + 3, s.yCoordinate + 3, s.xCoordinate + 38, s.yCoordinate + 38, 1, 1);
+        if ((event.offsetX >= s.xCoordinate && event.offsetX <= s.xCoordinate + 40) && (event.offsetY >= s.yCoordinate && event.offsetY <= s.yCoordinate + 40)) {
+            s.makeColorGrid(s.xCoordinate + 4, s.yCoordinate + 4, s.xCoordinate + 37, s.yCoordinate + 37, 1, 1);
         }
     }
 });
--- eventHandler.ts
+++ eventHandler.ts
@@ -1,21 +1,18 @@
 let currentColor = document.querySelector("#currentColor") as HTMLParagraphElement;
 
-
-
 window.addEventListener("load", function () {
     let grid : Square = squaresList[0];
     grid.replaceColor("white");
     grid.makeColorGrid(0, 0, canvas.height, canvas.width, 40);
     grid.makeColorGrid(0, 0, canvas.height, canvas.width, 120, 3);
+    grid.replaceColor("red");
     
 });
 
 canvas.addEventListener("click", function (event: MouseEvent){
     for (const s of squaresList) {
-        if ((event.offsetX >= s.xCoordinate - 40 && event.offsetX <= s.xCoordinate) && (event.offsetY >= s.yCoordinate - 40 && event.offsetY <= s.yCoordinate)){
-            s.makeColorGrid(s.xCoordinate - 37, s.yCoordinate - 37, s.xCoordinate - 2, s.yCoordinate - 2, 1, 1);
-        } else if ((event.offsetX >= s.xCoordinate && event.offsetX <= s.xCoordinate + 40) && (event.offsetY >= s.yCoordinate && event.offsetY <= s.yCoordinate + 40)){
-            s.makeColorGrid(s.xCoordinate + 3, s.yCoordinate + 3, s.xCoordinate + 38, s.yCoordinate + 38, 1, 1);
+        if ((event.offsetX >= s.xCoordinate && event.offsetX <= s.xCoordinate + 40) && (event.offsetY >= s.yCoordinate && event.offsetY <= s.yCoordinate + 40)){
+            s.makeColorGrid(s.xCoordinate + 4, s.yCoordinate + 4, s.xCoordinate + 37, s.yCoordinate + 37, 1, 1);
         }
     }
 });
