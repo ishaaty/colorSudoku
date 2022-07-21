c8ebb9847aa6575e3845c120864bd13a77cc269b
ishaaty <ishatyagi888@gmail.com>
2022-07-20 20:12:24 -0400

    got rid of allCoordinates

--- eventHandler.ts
+++ eventHandler.ts
@@ -11,17 +11,11 @@ let box : string = "box";
 let allCoordinates = []
 let squaresList : any = [];
 
-for(let x = 0; x < canvas.width; x += 40){
-    for (let y = 0; y < canvas.height; y += 40){
-        allCoordinates.push([x, y]);
-    }
-}
-
-console.log(allCoordinates);
-
 
 for(let x = 0; x < canvas.width - 1; x += 40){
     for (let y = 0; y < canvas.height - 1; y += 40){
+        allCoordinates.push([x, y]);
+
         (oneToNine === 0)? oneToNine = 1 : oneToNine *= 1;
 
         // column
@@ -99,14 +93,17 @@ for(let x = 0; x < canvas.width - 1; x += 40){
 console.log(squaresList);
 
 window.addEventListener("load", function () {
-    makeColorGrid(0, 0, canvas.height, canvas.width, 40);
-    makeColorGrid(0, 0, canvas.height, canvas.width, 120, 3);
+    let grid : Square = squaresList[0];
+    grid.makeColorGrid(0, 0, canvas.height, canvas.width, 40);
+    grid.makeColorGrid(0, 0, canvas.height, canvas.width, 120, 3);
 });
 
 canvas.addEventListener("click", function (event: MouseEvent){
-    for (const c of allCoordinates) {
-        if ((event.offsetX >= c[0] - 40 && event.offsetX <= c[0]) && (event.offsetY >= c[1] - 40 && event.offsetY <= c[1])){
-            makeColorGrid(c[0] - 37, c[1] - 37, c[0] - 2, c[1] - 2, 1, 1);
+    for (const s of squaresList) {
+        if ((event.offsetX >= s.xCoordinate - 40 && event.offsetX <= s.xCoordinate) && (event.offsetY >= s.yCoordinate - 40 && event.offsetY <= s.yCoordinate)){
+            s.makeColorGrid(s.xCoordinate - 37, s.yCoordinate - 37, s.xCoordinate - 2, s.yCoordinate - 2, 1, 1);
+        } else if ((event.offsetX >= s.xCoordinate && event.offsetX <= s.xCoordinate + 40) && (event.offsetY >= s.yCoordinate && event.offsetY <= s.yCoordinate + 40)){
+            s.makeColorGrid(s.xCoordinate + 3, s.yCoordinate + 3, s.xCoordinate + 38, s.yCoordinate + 38, 1, 1);
         }
     }
 });
@@ -116,38 +113,7 @@ document.querySelectorAll<HTMLElement>(".colorBtn").forEach(function(elm){
     elm.addEventListener("click", function (event : MouseEvent){
         let clickedColor = event.currentTarget as HTMLElement;
         currentColor.textContent = (`Current Color: ${clickedColor.dataset.key}`);
-        for (const c of allCoordinates){
-            color = clickedColor.dataset.key as string;
-        }
+        color = clickedColor.dataset.key as string;
     })
 });
 
-
-
-
-function makeColorGrid(xMin : number, yMin : number, xMax : number, yMax : number, inc : number, width : number = 1) {
-    // making vertical lines
-    for (let xUpdate = xMin; xUpdate < xMax; xUpdate += inc){
-        for (let yUpdate = yMin; yUpdate <= yMax; yUpdate++){
-                fillCircle(xUpdate, yUpdate, width);
-        
-        }
-    }
-
-    // making horizontal lines
-    for (let yUpdate = yMin; yUpdate < yMax; yUpdate += inc){ 
-        for (let xUpdate = xMin; xUpdate < xMax; xUpdate++){
-                fillCircle(xUpdate, yUpdate, width);
-            
-        }
-    }
-}
-
-
-function fillCircle(x : number, y : number, width : number = 1) : void {
-    ctx.beginPath();
-    ctx.fillStyle = color;
-    ctx.arc(x, y, width, 0, Math.PI*2);
-    ctx.fill();
-    ctx.closePath();
-}
